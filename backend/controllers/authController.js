d612fb0671262c1839b60ebcd17c42464a3a4a0e
SubhamUpadhyay <upadhyaysubham325@gmail.com>
2025-10-14 15:57:02 +0530

    login controller completed

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -29,12 +29,40 @@ const register = async(req,res)=>{
         
     }catch(err)
     {
-        return res.status(500).json({error:"Registration Failed"});
+        return res.status(500).json({error:"Registration Failed"+err.message});
     }
 }
 
 
 
 
+const login = async(req,res)=>{
+    try{
+        const{email,password} = req.body;
+        if(!email || !password)
+        {
+            return res.status(400).json({error:"Email & Password both required"});
+        }
+        const user = await User.findOne({email});
+        if(!user)   
+        {
+            return res.status(401).json({error:"User not found . please register"});
+        }
+        const isPassword = await bcrypt.compare(password,user.password);
+        if(!isPassword)
+        {
+            return res.status(401).json({error:"Invalid Credentials"});
+        }
+
+        const token = jwt.sign({userId:user._id,role:user.role},process.env.JWT_SECRET,{expiresIn:'7d'});
+        return res.status(200).json({message:"Login Successful"});
+    }  
+    catch(err)
+    {
+        res.status(500).json({error:"Login Failed"+err.message});
+    } 
+}
 
 
+
+module.exports = {register,login};
