4bd07b102462d8da29b01fc3b4ce74de2ddc8819
SubhamUpadhyay <upadhyaysubham325@gmail.com>
2025-10-14 12:52:51 +0530

    Modelling completed

---
+++ backend/models/movieModel.js
@@ -0,0 +1,48 @@
+const mongoose =  require("mongoose")
+// const {schema} = mongoose;
+
+
+
+const movieSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    casts:{
+        type:[String],
+        required:true
+    },
+    trailerUrl:{
+        type:String,
+        required:true
+    },
+    langugae:{
+        type:[String],
+        required:true,
+        default:"English"
+    },
+    releaseDate:{
+        type:String,
+        require:true
+    },
+    director:{
+        type:String,
+        required:true
+    },
+    releaseStatus:{
+        type:String,
+        required:true,
+        default:"RELEASED"
+    }
+},{timestamps:true});
+
+
+
+const Movie = mongoose.model("Movie",movieSchema); //creates a new model 
+
+module.exports = Movie; //return the model
+
