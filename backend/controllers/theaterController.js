0c8be6e58d67cee9829d27a37c273ab3283a7be0
SubhamUpadhyay <upadhyaysubham325@gmail.com>
2025-10-14 16:59:04 +0530

    Theater controller

---
+++ backend/controllers/theaterController.js
@@ -0,0 +1,32 @@
+const Theater = require("../models/theaterModel");
+const createTheater = async(req,res)=>{
+    try{
+        const theater =await Theater.create(req.body);
+        return res.status(201).json({message:"Theater Created Successfully"});
+    }catch(err)
+    {
+        return res.status(500).json({error:err.message});
+    }
+}
+
+
+const getTheaterByCity = async(req,res)=>{
+    try{
+        const {city} = req.params;
+        const theaters = await Theater.find({city});
+        return res.status(200).json({theaters});
+    }catch(err)
+    {
+        return res.status(500).json({error:err.message});
+    }
+}
+
+const getAllTheaters = async(req,res)=>{
+    try{
+        const theaters = await Theater.find();
+        return res.status(200).json({theaters});
+    }catch(err)
+    {
+        return res.status(500).json({error:err.message});
+    }
+}
